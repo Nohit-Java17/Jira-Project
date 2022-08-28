565a587e7f3f218f4893c4df63e4a21d4c3c4335
TênMac <caoducmanh2411999@gmail.com>
2022-08-28 09:43:51 +0300

    - Add file webconfig - By Mac Mie

---
+++ src/main/java/com/nohit/jira_project/constant/ViewConstant.java
@@ -0,0 +1,6 @@
+package com.nohit.jira_project.constant;
+
+public class ViewConstant {
+    public static final String INDEX_VIEW = "/index";
+    public static final String NOT_FOUND_VIEW = "/404";
+}
