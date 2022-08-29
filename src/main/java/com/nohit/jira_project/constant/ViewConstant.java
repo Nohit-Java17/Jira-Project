3be9abd897fc2dbe9baa915404cfcab896be0bf4
Nguyễn Tiến Đạt <tiendat.8498@gmail.com>
2022-08-30 00:37:01 +0700

    #24 temple, view constant

--- src/main/java/com/nohit/jira_project/constant/ViewConstant.java
+++ src/main/java/com/nohit/jira_project/constant/ViewConstant.java
@@ -1,6 +1,14 @@
 package com.nohit.jira_project.constant;
 
 public class ViewConstant {
-    public static final String INDEX_VIEW = "/index";
-    public static final String NOT_FOUND_VIEW = "/404";
+    public static final String INDEX_VIEW 			= "/index";
+    public static final String NOT_FOUND_VIEW 		= "/404";
+    public static final String HOME_VIEW 			= "/home";
+    public static final String SHOP_VIEW			= "/shop";
+    public static final String CATEGORY_VIEW 		= "/category";
+    public static final String SINGLE_PRODUCT_VIEW 	= "/single-product";
+    public static final String CART_VIEW 			= "/cart";
+    public static final String CHECKOUT_VIEW 		= "/checkout";
+    public static final String ABOUT_VIEW 			= "/about";
+    public static final String CONTACT_VIEW 		= "/contact";
 }
