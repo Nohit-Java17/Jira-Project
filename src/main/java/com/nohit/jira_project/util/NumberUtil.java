198c5c071faa32a9b17c29d3ae0c27de8ed942a6
Yami An <yamiannephilim@gmail.com>
2022-08-30 21:06:43 +0700

    #29 update util

---
+++ src/main/java/com/nohit/jira_project/util/NumberUtil.java
@@ -0,0 +1,18 @@
+package com.nohit.jira_project.util;
+
+import org.springframework.stereotype.*;
+
+import static java.lang.Integer.*;
+
+@Component
+public class NumberUtil {
+    // Check string is number or not
+    public boolean isNumeric(String s) {
+        try {
+            parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
