a9ff4f069a9da858ec86e3a2f307c23ef6b53635
Yami An <yamiannephilim@gmail.com>
2022-09-01 10:17:23 +0700

    review repository
    Co-authored-by: tiendat-NTD <tiendat.8498@gmail.com>
    Co-authored-by: macmie2411999 <caoducmanh2411999@gmail.com>

--- src/main/java/com/nohit/jira_project/service/Impl/KhachHangServiceImpl.java
+++ src/main/java/com/nohit/jira_project/service/Impl/KhachHangServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import javax.transaction.*;
 
-import org.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.authority.*;
 import org.springframework.security.core.userdetails.*;
@@ -15,21 +14,20 @@ import com.nohit.jira_project.model.*;
 import com.nohit.jira_project.repository.*;
 import com.nohit.jira_project.service.*;
 
+import lombok.extern.slf4j.*;
+
 import static com.nohit.jira_project.constant.AttributeConstant.*;
 import static java.util.Collections.*;
-import static org.slf4j.LoggerFactory.*;
 
 @Service
 @Transactional
+@Slf4j
 public class KhachHangServiceImpl implements KhachHangService, UserDetailsService {
     @Autowired
     private KhachHangRepository khachHangRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    // Fields
-    private Logger log = getLogger(KhachHangServiceImpl.class);
 
     @Override
     public List<KhachHang> getDsKhachHang() {
@@ -46,7 +44,7 @@ public class KhachHangServiceImpl implements KhachHangService, UserDetailsServic
     @Override
     public KhachHang getKhachHang(String email) {
         // TODO Auto-generated method stub
-        return khachHangRepository.findByemail(email);
+        return khachHangRepository.findByEmail(email);
     }
 
     @Override
@@ -64,7 +62,7 @@ public class KhachHangServiceImpl implements KhachHangService, UserDetailsServic
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // Query data of user
-        var user = khachHangRepository.findByemail(username);
+        var user = khachHangRepository.findByEmail(username);
         // Check user exists
         if (user == null) {
             log.error("User not found");
