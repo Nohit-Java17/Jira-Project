4d773c46a2c93b56da123d3ec57d73ad746cb4ff
Yami An <yamiannephilim@gmail.com>
2022-09-10 19:40:45 +0700

    fix cart fe
    Co-authored-by: tiendat-NTD <tiendat.8498@gmail.com>
    Co-authored-by: macmie2411999 <caoducmanh2411999@gmail.com>

--- src/main/java/com/nohit/jira_project/controller/PhanLoaiController.java
+++ src/main/java/com/nohit/jira_project/controller/PhanLoaiController.java
@@ -30,11 +30,42 @@ public class PhanLoaiController {
     public ModelAndView category() {
         var mav = new ModelAndView(CATEGORY_TEMP);
         var khachHang = authenticationUtil.getAccount();
+        var dsTonkho = sanPhamService.getDsSanPhamTonKho();
+        var maxDsTonkho = dsTonkho.size();
         mav.addObject("cart", applicationUtil.getOrDefaultGioHang(khachHang));
         mav.addObject("choosenOne", PHAN_LOAI);
         mav.addObject("login", khachHang != null);
-        mav.addObject("products", sanPhamService.getDsSanPhamTonKho());
+        mav.addObject("products",
+                dsTonkho.subList(0, maxDsTonkho < DEFAULT_SIZE_PAGE ? maxDsTonkho : DEFAULT_SIZE_PAGE));
         mav.addObject("radioCheck", DEFAULT_CATEGORY);
+        mav.addObject("maxSize", (dsTonkho.size() - 1) / DEFAULT_SIZE_PAGE + 1);
+        mav.addObject("view", PAGE_VIEW + "?page=");
+        mav.addObject("previous", PAGE_VIEW + "?page=" + 1);
+        mav.addObject("next", PAGE_VIEW + "?page=" + 2);
+        return mav;
+    }
+
+    // Load category from page
+    @GetMapping(PAGE_VIEW)
+    public ModelAndView category(int page) {
+        var mav = new ModelAndView(CATEGORY_TEMP);
+        var khachHang = authenticationUtil.getAccount();
+        var dsTonkho = sanPhamService.getDsSanPhamTonKho();
+        var maxDsTonkho = dsTonkho.size();
+        var maxPage = page * DEFAULT_SIZE_PAGE;
+        var maxSize = (maxDsTonkho - 1) / DEFAULT_SIZE_PAGE + 1;
+        var previous = page - 1;
+        var next = page + 1;
+        mav.addObject("cart", applicationUtil.getOrDefaultGioHang(khachHang));
+        mav.addObject("choosenOne", PHAN_LOAI);
+        mav.addObject("login", khachHang != null);
+        mav.addObject("products",
+                dsTonkho.subList((page - 1) * DEFAULT_SIZE_PAGE, maxPage > maxDsTonkho ? maxDsTonkho : maxPage));
+        mav.addObject("radioCheck", DEFAULT_CATEGORY);
+        mav.addObject("maxSize", maxSize);
+        mav.addObject("view", PAGE_VIEW + "?page=");
+        mav.addObject("previous", PAGE_VIEW + "?page=" + (previous < 1 ? 1 : previous));
+        mav.addObject("next", PAGE_VIEW + "?page=" + (next > maxSize ? maxSize : next));
         return mav;
     }
 
@@ -43,11 +74,44 @@ public class PhanLoaiController {
     public ModelAndView categoryFilter(String filter) {
         var mav = new ModelAndView(CATEGORY_TEMP);
         var khachHang = authenticationUtil.getAccount();
+        var dsTonkho = sanPhamService.getDsSanPham(filter);
+        var maxDsTonkho = dsTonkho.size();
         mav.addObject("cart", applicationUtil.getOrDefaultGioHang(khachHang));
         mav.addObject("choosenOne", PHAN_LOAI);
         mav.addObject("login", khachHang != null);
-        mav.addObject("products", sanPhamService.getDsSanPham(filter));
+        mav.addObject("products",
+                dsTonkho.subList(0, maxDsTonkho < DEFAULT_SIZE_PAGE ? maxDsTonkho : DEFAULT_SIZE_PAGE));
         mav.addObject("radioCheck", CATEGORIES_MAP.get(filter));
+        mav.addObject("maxSize", (dsTonkho.size() - 1) / DEFAULT_SIZE_PAGE + 1);
+        mav.addObject("view", FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=");
+        mav.addObject("previous", FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=" + 1);
+        mav.addObject("next", FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=" + 2);
+        return mav;
+    }
+
+    // Load filter products from page
+    @GetMapping(FILTER_VIEW + PAGE_VIEW)
+    public ModelAndView categoryFilter(String filter, int page) {
+        var mav = new ModelAndView(CATEGORY_TEMP);
+        var khachHang = authenticationUtil.getAccount();
+        var dsTonkho = sanPhamService.getDsSanPham(filter);
+        var maxDsTonkho = dsTonkho.size();
+        var maxPage = page * DEFAULT_SIZE_PAGE;
+        var maxSize = (maxDsTonkho - 1) / DEFAULT_SIZE_PAGE + 1;
+        var previous = page - 1;
+        var next = page + 1;
+        mav.addObject("cart", applicationUtil.getOrDefaultGioHang(khachHang));
+        mav.addObject("choosenOne", PHAN_LOAI);
+        mav.addObject("login", khachHang != null);
+        mav.addObject("products",
+                dsTonkho.subList((page - 1) * DEFAULT_SIZE_PAGE, maxPage > maxDsTonkho ? maxDsTonkho : maxPage));
+        mav.addObject("radioCheck", CATEGORIES_MAP.get(filter));
+        mav.addObject("maxSize", maxSize);
+        mav.addObject("view", FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=");
+        mav.addObject("previous",
+                FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=" + (previous < 1 ? 1 : previous));
+        mav.addObject("next",
+                FILTER_VIEW + PAGE_VIEW + "?filter=" + filter + "&page=" + (next > maxSize ? maxSize : next));
         return mav;
     }
 }
