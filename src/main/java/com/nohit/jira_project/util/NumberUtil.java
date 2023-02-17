d05574a80f8f2ca3e83ce27b271e1379d9bf852d
Yami An <yamiannephilim@gmail.com>
2023-02-17 22:17:46 +0700

    add isFloat method
    Co-authored-by: tiendat-NTD <tiendat.8498@gmail.com>
    Co-authored-by: macmie2411999 <caoducmanh2411999@gmail.com>

--- src/main/java/com/nohit/jira_project/util/NumberUtil.java
+++ src/main/java/com/nohit/jira_project/util/NumberUtil.java
@@ -3,13 +3,24 @@ package com.nohit.jira_project.util;
 import org.springframework.stereotype.*;
 
 import static java.lang.Integer.*;
+import static java.lang.Float.*;
 
 @Component
 public class NumberUtil {
-    // Check string is number or not
-    public boolean isNumeric(String s) {
+    // Check string is integer or not
+    public boolean isInt(String s) {
         try {
             parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Check string is float or not
+    public boolean isFloat(String s) {
+        try {
+            parseFloat(s);
             return true;
         } catch (NumberFormatException e) {
             return false;
